e3bcfd61dda6ceae9339d76ad32339baa8bc6656
Hittesh Ahuja <hittesh.ahuja@iris-worldwide.com>
2025-03-13 10:39:38 +0000

    modified eslint config to continue buid

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ['next', 'next/typescript'],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      'next/typescript': 'off',
     },
   }),
 ]
