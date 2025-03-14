393fc2864275d2bd55a02458fd65ee33d2f31877
Hittesh Ahuja <hittesh.ahuja@iris-worldwide.com>
2025-03-14 09:55:33 +0000

    removed user page

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,10 +1,3 @@
 
 // File path: /Users/Hittesh.Ahuja/Projects/nextjsprojects/youtube-nextjs-15-app/app/dashboard/users/page.tsx
 // Content:
-const Page = ({ params }: { params: { id: string } }) => {
-    const { id } = params;
-    return (
-        <div>Users page {id}</div>
-    );
-}
-export default Page;
